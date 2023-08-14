52a215482ffa565ce2d9c94d5e4d1dabffd85746
nanaon <naon.jeong@gmail.com>
2023-08-14 23:32:31 +0900

    Set initial settings of spring boot project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # java-practice-board
+
 자바, 스프링부트로 게시판 만들기 연습
---
+++ src/test/java/com/practice/javapracticeboard/JavaPracticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.javapracticeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPracticeBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
