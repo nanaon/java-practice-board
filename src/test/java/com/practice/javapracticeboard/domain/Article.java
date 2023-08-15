39828a6e831643292de070b39a5048afc423850f
nanaon <naon.jeong@gmail.com>
2023-08-15 14:03:14 +0900

    Removed unnecessary whitespace

--- src/test/java/com/practice/javapracticeboard/domain/Article.java
+++ src/test/java/com/practice/javapracticeboard/domain/Article.java
@@ -11,5 +11,5 @@ public class Article {
     private LocalDateTime createdAt; // 생성일시
     private String createdBy; // 작성자
     private LocalDateTime modifiedAt; // 수정일시
-    private  String modifiedBy; // 수정자
+    private String modifiedBy; // 수정자
 }
