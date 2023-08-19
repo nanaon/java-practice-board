0ffba99218b1a6f3eefcc9a99f768364fff346e5
nanaon <naon.jeong@gmail.com>
2023-08-19 16:10:16 +0900

    Configured Thymeleaf3 decoupled logic

--- src/main/java/com/practice/javapracticeboard/JavaPracticeBoardApplication.java
+++ src/main/java/com/practice/javapracticeboard/JavaPracticeBoardApplication.java
@@ -2,7 +2,9 @@ package com.practice.javapracticeboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class JavaPracticeBoardApplication {
 
