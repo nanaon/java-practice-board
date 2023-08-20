44eadba0b5d2d8b8a39ac1aebb241b3f1a99c62d
nanaon <naon.jeong@gmail.com>
2023-08-20 15:54:18 +0900

    Modified security configuration to allow unauthenticated access for all requests

--- src/main/java/com/practice/javapracticeboard/config/SecurityConfig.java
+++ src/main/java/com/practice/javapracticeboard/config/SecurityConfig.java
@@ -1,7 +1,21 @@
 package com.practice.javapracticeboard.config;
 
-public class SecurityConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(login -> login
+                        .defaultSuccessUrl("/articles").permitAll());
+
+        return http.build();
+    }
 
 }
